1a6b2ea0f974a65057e402bd8e080a1cf9d4a878
Boyi Wei <boyiw7@uci.edu>
2023-04-12 11:28:36 -0700

    Finihsed sort_top.c

--- sort_top.c
+++ sort_top.c
@@ -4,3 +4,11 @@
 #include "batch_size.h"
 
 
+
+void sort_top(int input[2][batch_size], int output[2*batch_size]){
+	int half_sorted[2][batch_size];
+	void radix_sort_unified_bucket(input[1], half_sorted[1]);
+	void radix_sort_unified_bucket(input[2], half_sorted[2]);
+
+	void merge_sort(half_sorted[1], half_sorted[2], output);
+}
